682b92f6172181669b2694a9c2e67e8102d1a21f
datarka5 <37141714+datarka5@users.noreply.github.com>
2022-09-23 15:08:42 +0200

    title changed

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ from streamlit_profiler import Profiler
 
 
 # from streamlit_option_menu import option_menu
-st.set_page_config(page_title="Global Sales Block", layout="wide", page_icon="chart_with_upwards_trend")
+st.set_page_config(page_title="Streamlit cloud testing", layout="wide", page_icon="chart_with_upwards_trend")
 
-st.title("Global Sales Block")
-# https://brocoders.com/blog/mapbox-vs-google-maps-vs-openstreetmap/#:~:text=With%20Google%20Maps%20and%20Mapbox,all%20the%20bells%20and%20whistles.
+st.title("Streamlit cloud testing")
 
 tab_about, tab_tutorial = st.tabs(["About", "Tutorials"])
